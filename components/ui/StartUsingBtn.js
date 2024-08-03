a631ce48fbaff121756c1d6e2d3d44b68017be0c
AaronHsieh <piafyoyo06@gmail.com>
2024-08-03 23:34:42 +0800

    做出開始使用按鈕

--- components/ui/StartUsingBtn.js
+++ components/ui/StartUsingBtn.js
@@ -2,6 +2,17 @@ import Link from "next/link";
 
 import styles from "./StartUsingBtn.module.css";
 
-export default function StartUsingBtn() {
-  return <Link></Link>;
+export default function StartUsingBtn({ withIcon = false }) {
+  return (
+    <Link href="/pricing" className={styles.button}>
+      開始使用
+      {withIcon && (
+        <img
+          src="/image/icons/call made.webp"
+          alt="行動呼籲"
+          className={styles.icon}
+        />
+      )}
+    </Link>
+  );
 }
