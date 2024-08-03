15262100c9e91a1bf623fca51f6c7591a65b6ffe
AaronHsieh <piafyoyo06@gmail.com>
2024-08-03 23:51:24 +0800

    完成圖案動畫區塊

--- components/ui/DecoAnimation.js
+++ components/ui/DecoAnimation.js
@@ -1,5 +1,25 @@
 import styles from "./DecoAnimation.module.css";
 
-export default function DecoAnimation() {
-  return <section></section>;
+export default function DecoAnimation({ align }) {
+  // 根據傳入的 align prop 動態設置 container 的樣式
+  const containerClass = `${styles.container} ${
+    align === "start" ? styles["flex-start"] : ""
+  } ${align === "end" ? styles["flex-end"] : ""}`;
+
+  return (
+    <section className={containerClass}>
+      <img
+        src="image/animation/deco.webp"
+        className={`${styles.deco1} ${styles["hide-deco"]}`}
+      />
+      <img
+        src="image/animation/deco.webp"
+        className={`${styles.deco2} ${styles["hide-deco"]}`}
+      />
+      <img
+        src="image/animation/deco.webp"
+        className={`${styles.deco3} ${styles["hide-deco"]}`}
+      />
+    </section>
+  );
 }
