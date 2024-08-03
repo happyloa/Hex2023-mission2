754b28b380018a6611d44f31e5fd0854cbe61b29
AaronHsieh <piafyoyo06@gmail.com>
2024-08-04 00:29:41 +0800

    完成我們的服務介紹卡片

--- components/ui/ServicesCard.js
+++ components/ui/ServicesCard.js
@@ -1,5 +1,11 @@
 import styles from "./ServicesCard.module.css";
 
-export default function ServicesCard() {
-  return <li></li>;
+export default function ServicesCard({ src, title, description }) {
+  return (
+    <li className={styles.card}>
+      <img src={src} alt={`${title} icon`} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
 }
