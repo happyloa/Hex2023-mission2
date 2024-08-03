9d2164955038270268f9e6a601afa7a268ad6403
AaronHsieh <piafyoyo06@gmail.com>
2024-08-03 23:13:01 +0800

    引入並使用 Header 與 Footer

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,9 @@ const notoSansTC = Noto_Sans_TC({
   display: "swap",
 });
 
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+
 export const metadata = {
   title: "2023 體驗營切版任務二",
   description: "2023 體驗營切版任務二 by Aaron",
@@ -15,7 +18,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="zh-Hant" className={notoSansTC.className}>
-      <body>{children}</body>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
