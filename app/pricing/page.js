8181709295837fb0397da837eb0103cf2d360d7f
AaronHsieh <piafyoyo06@gmail.com>
2024-08-03 23:09:57 +0800

    新增定價頁面 router link

---
+++ app/pricing/page.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "定價 - 2023 體驗營切版任務二",
+};
+
+import PricingTitle from "@/components/pricing/PricingTitle";
+
+export default function Pricing() {
+  return (
+    <>
+      <PricingTitle />
+    </>
+  );
+}
