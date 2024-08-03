40f4716a82ce91cbf9a285fd25eecbc95c9007b2
AaronHsieh <piafyoyo06@gmail.com>
2024-08-03 23:10:16 +0800

    引入標題元件

--- app/page.js
+++ app/page.js
@@ -2,6 +2,12 @@ export const metadata = {
   title: "首頁 - 2023 體驗營切版任務二",
 };
 
+import HomeTitle from "@/components/home/HomeTitle";
+
 export default function Home() {
-  return <div>123</div>;
+  return (
+    <>
+      <HomeTitle />
+    </>
+  );
 }
