5a76971d3a13c15cc9e8f9608d00dd2efe1ea12f
AaronHsieh <piafyoyo06@gmail.com>
2024-08-03 23:20:07 +0800

    新增首頁標題內容

--- components/home/HomeTitle.js
+++ components/home/HomeTitle.js
@@ -2,8 +2,18 @@ import styles from "./HomeTitle.module.css";
 
 export default function HomeTitle() {
   return (
-    <section>
-      <h1></h1>
+    <section className={styles.container}>
+      <h1 className={styles.title}>
+        透過
+        <br />
+        AI 工具王的
+        <br />
+        強大模型
+        <br />
+        讓您的業務
+        <br className={styles["responsive-break"]} />
+        更聰明
+      </h1>
     </section>
   );
 }
