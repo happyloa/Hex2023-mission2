fa9b1db11e0a0d57ea2d4d6fc0bcd69b1f24aa2d
AaronHsieh <piafyoyo06@gmail.com>
2024-08-04 18:39:48 +0800

    完成首頁行動呼籲區塊

--- components/home/CTA.js
+++ components/home/CTA.js
@@ -1,5 +1,16 @@
 import styles from "./CTA.module.css";
 
+import StartUsingBtn from "../ui/StartUsingBtn";
+
 export default function CTA() {
-  return <section></section>;
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.heading}>
+        現在就來建立
+        <br className={styles["responsive-break"]} />
+        屬於你的服務吧
+      </h2>
+      <StartUsingBtn withIcon />
+    </section>
+  );
 }
