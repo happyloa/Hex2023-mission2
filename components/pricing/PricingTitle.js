742216ff074e038d8bb9925f230505dd49a9238e
AaronHsieh <piafyoyo06@gmail.com>
2024-08-04 20:09:02 +0800

    完成定價頁面標題區塊

--- components/pricing/PricingTitle.js
+++ components/pricing/PricingTitle.js
@@ -2,8 +2,15 @@ import styles from "./PricingTitle.module.css";
 
 export default function PricingTitle() {
   return (
-    <section>
-      <h1></h1>
+    <section className={styles.container}>
+      <hgroup className={styles["content-container"]}>
+        <h1>
+          用多少，
+          <br />
+          付多少。
+        </h1>
+        <p>我們相信，最靈活的取用機制，才能最大化的幫助你業務的推動。</p>
+      </hgroup>
     </section>
   );
 }
