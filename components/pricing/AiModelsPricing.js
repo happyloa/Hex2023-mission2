df18455c3db7250132840cfb25b6fe9944bb3701
AaronHsieh <piafyoyo06@gmail.com>
2024-08-04 21:03:31 +0800

    完成定價元件區塊

---
+++ components/pricing/AiModelsPricing.js
@@ -0,0 +1,43 @@
+import styles from "./AiModelsPricing.module.css";
+
+import AiModelsPricingCard from "../ui/AiModelsPricingCard";
+
+const aiModelsInfo = [
+  {
+    title: "琪琪",
+    features: ["初級", "速度最快"],
+    price: 5,
+    unit: "1k tokens",
+  },
+  {
+    title: "昊昊",
+    features: ["中級"],
+    price: 5,
+    unit: "1k tokens",
+  },
+  {
+    title: "卡卡",
+    features: ["高級"],
+    price: 5,
+    unit: "1k tokens",
+  },
+  {
+    title: "杰杰",
+    features: ["最高級", "資訊最完善"],
+    price: 5,
+    unit: "1k tokens",
+  },
+];
+
+export default function AiModelsPricing() {
+  return (
+    <section className={styles.container}>
+      <h2 className={styles.heading}>定價</h2>
+      <ul className={styles["card-wrapper"]}>
+        {aiModelsInfo.map((data, index) => (
+          <AiModelsPricingCard key={index} {...data} />
+        ))}
+      </ul>
+    </section>
+  );
+}
