33e3acd232f7ee2594d9f3e0240aff47f8be7a63
AaronHsieh <piafyoyo06@gmail.com>
2024-08-04 17:17:21 +0800

    完成客戶見證卡片

--- components/ui/TestimonialCard.js
+++ components/ui/TestimonialCard.js
@@ -1,5 +1,29 @@
 import styles from "./TestimonialCard.module.css";
 
-export default function TestimonialCard() {
-  return <li></li>;
+export default function TestimonialCard({
+  rating,
+  content,
+  clientName,
+  clientCompany,
+  clientImage,
+}) {
+  return (
+    <li className={styles.card}>
+      <article className={styles.wrapper}>
+        <div className={styles["rating-wrapper"]}>
+          {Array.from({ length: rating }, (_, index) => (
+            <img key={index} src="image/icons/star.webp" />
+          ))}
+        </div>
+        <p className={styles.content}>{content}</p>
+        <div className={styles["client-wrapper"]}>
+          <img src={clientImage} />
+          <div className={styles["client-info"]}>
+            <strong>{clientName}</strong>
+            <span>{clientCompany}</span>
+          </div>
+        </div>
+      </article>
+    </li>
+  );
 }
