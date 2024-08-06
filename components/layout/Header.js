5a783462e33f4076e2802e1be74c909ba0fec066
AaronHsieh <piafyoyo06@gmail.com>
2024-08-06 13:51:42 +0800

    新增電腦版導覽選單

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -3,5 +3,17 @@ import Link from "next/link";
 import styles from "./Header.module.css";
 
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className={styles.container}>
+      <div className={styles.wrapper}>
+        <Link href="/">
+          <img src="image/logo.webp" className={styles["site-logo"]} />
+        </Link>
+        <nav className={styles["desktop-nav"]}>
+          <Link href="/">首頁</Link>
+          <Link href="/pricing">定價</Link>
+        </nav>
+      </div>
+    </header>
+  );
 }
