4cedb6c185335c2fbf03880573d7809a321aefd3
AaronHsieh <piafyoyo06@gmail.com>
2024-08-08 13:10:15 +0800

    修正將 children 作為 props 的錯誤

--- components/ui/SingleAccordion.js
+++ components/ui/SingleAccordion.js
@@ -3,5 +3,5 @@ import { Accordion } from "react-bootstrap-accordion";
 import "react-bootstrap-accordion/dist/index.css";
 
 export default function SingleAccordion({ title, content }) {
-  return <Accordion title={title} children={content} />;
+  return <Accordion title={title}>{content}</Accordion>;
 }
