0b3579947543b1e2ceac45cc2a237b5377d0bb21
AaronHsieh <piafyoyo06@gmail.com>
2024-08-08 13:16:58 +0800

    修正渲染時出現的錯誤

--- components/ui/SingleAccordion.js
+++ components/ui/SingleAccordion.js
@@ -5,5 +5,8 @@ import { Accordion } from "react-bootstrap-accordion";
 import "react-bootstrap-accordion/dist/index.css";
 
 export default function SingleAccordion({ title, content }) {
-  return <Accordion title={title}>{content}</Accordion>;
+  if (typeof window !== "undefined") {
+    return <Accordion title={title}>{content}</Accordion>;
+  }
+  return null;
 }
