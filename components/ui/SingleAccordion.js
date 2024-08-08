4e9870fd848201aeb30e879b237d521cdfb2939a
AaronHsieh <piafyoyo06@gmail.com>
2024-08-08 13:05:02 +0800

    新增單個 faq 元件

---
+++ components/ui/SingleAccordion.js
@@ -0,0 +1,7 @@
+import { Accordion } from "react-bootstrap-accordion";
+
+import "react-bootstrap-accordion/dist/index.css";
+
+export default function SingleAccordion({ title, content }) {
+  return <Accordion title={title} children={content} />;
+}
